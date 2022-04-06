331afdb373facaea64fb39d7b8e187c5b20b6eda
maciej <maciejsz0@o2.pl>
2022-04-06 22:45:32 +0200

    service

---
+++ src/app/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private users = [
+    {'id': 0, email: 'jan.kowalski@gmail.com', password: 'test1234'},
+    {'id': 1, email: 'ewa.kowalska@gmail.com', password: 'test1234'},
+    {'id': 2, email: 'nowak.marcin@o2.pl', password: 'test5678'},
+    {'id:' 3, email: 'nowak.edyta@o2.pl', password: 'test5678'}
+  ]
+  private loggedUser = {};
+
+  constructor() { }
+
+  getUsers() {
+    return this.users;
+  }
+
+  getLoggedUser() {
+    return this.loggedUser;
+  }
+
+  setLoggedUser(loggedUser) {
+    this.loggedUser = loggedUser;
+  }
+}
