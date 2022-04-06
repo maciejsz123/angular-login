7750dddcc261aeaa842bbb1d84353b232ad207d4
maciej <maciejsz0@o2.pl>
2022-04-06 22:14:00 +0200

    added routes for account and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'konto', component: AccountComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
